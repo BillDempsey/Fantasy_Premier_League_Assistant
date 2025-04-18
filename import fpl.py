92f33ef16ec8cf78b78acaa3fe6fec86423d71cf
BillDempsey <62023498+BillDempsey@users.noreply.github.com>
2025-04-18 17:21:23 +0100

    Update import fpl.py
    Backup before I make changes

--- import fpl.py
+++ import fpl.py
@@ -1,21 +1,27 @@
 import requests
 import pandas as pd
 import json
-import decimal
 import functions
+import os
 
-# Make a request to GET the data from the FPL API
+file_names = ['24-25.xlsx', '23-24.xlsx', '22-23.xlsx', '21-22.xlsx', '20-21.xlsx', '19-20.xlsx']
+
+# Pull in the current FPL data and save it under the first file name i.e. '24-25.xlsx'
 url = 'https://fantasy.premierleague.com/api/bootstrap-static/'
 response = requests.get(url)
 data = json.loads(response.text)
-df = pd.DataFrame.from_dict(data['elements'])
+current_season_data = pd.DataFrame.from_dict(data['elements'])
+if os.path.exists(file_names[0]):
+    print(f"{file_names[0]} already exists and will be overwritten.")
+current_season_data.to_excel(file_names[1], index=False)
+print(f"Current season data has been saved to {file_names[0]}.")
 
-# Come back and make integers of all numerical columns
-df['points_per_game'] = df['points_per_game'].apply(decimal.Decimal)
-df['now_cost'] = df['now_cost'].apply(decimal.Decimal)/10
+dataframes = [pd.read_excel(file) for file in file_names]
 
-df['ppg_per_euro'] = df['points_per_game']/df['now_cost']
-df['ppg_per_euro'] = df['ppg_per_euro'].astype(float).round(3)
+for df in dataframes:
+    functions.sort_out_columns(df)
+
+functions.store_historical_data(*dataframes)
 
 teams = [
     "Arsenal",
@@ -23,26 +29,27 @@ teams = [
     "Bournemouth",
     "Brentford",
     "Brighton & Hove Albion",
-    "Burnley",
     "Chelsea",
     "Crystal Palace",
     "Everton",
     "Fulham",
+    "Ipswich Town",
+    "Leicester City",
     "Liverpool",
-    "Luton Town",
     "Manchester City",
     "Manchester United",
     "Newcastle United",
     "Nottingham Forest",
-    "Sheffield United",
+    "Southampton",
     "Tottenham Hotspur",
     "West Ham United",
     "Wolverhampton Wanderers"
 ]
 
-functions.choose_metric(df, teams)
+
+functions.choose_metric(teams, dataframes[0])   # Display top performers based on current season
 # print(df[df['ppg_per_euro'] != 0]['ppg_per_euro'].mean())
 # print(df['points_per_game'].mean())
-# functions.get_player_ranking(df, 'total_points', 'Palmer')
+#functions.get_player_ranking(df_current, 'total_points', 'Palmer')
 
 
